25cbb1d749c5472d55af3a31905661c96323cef7
Dmytro <dmitro.kurgan@gmail.com>
2022-11-28 21:21:38 +0200

    Add image cache

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 "use strict";
 
 var images = ['full-carezza-lake.jpg', 'full-lonely.jpg', 'full-uluwatu.jpg'];
+var src = './images/';
 var block = document.getElementById('bgImage');
 var handler = setInterval(carousel, 10000);
 var countEnd = 6;
 var startDelay = 1500;
 var count = 0;
 var imageIndex;
+images.forEach(function (img) {
+  new Image().src = src + img;
+});
 function getIndex() {
   localStorage.getItem('image') ? imageIndex = localStorage.getItem('image') : imageIndex = Math.floor(Math.random() * images.length);
 }
@@ -18,7 +22,7 @@ function carousel() {
     setTimeout(function () {
       block.style.opacity = '1';
     }, startDelay);
-    block.style.background = 'url("./images/' + images[imageIndex] + '") no-repeat center center fixed';
+    block.style.background = "url(\"".concat(src).concat(images[imageIndex], "\") no-repeat center center fixed");
     block.style.backgroundSize = 'cover';
   }
   imageIndex++;
